ef3b9e436061674532b5515dcab36c0a9529583a
Keira Garland <keiragarland@gmail.com>
2020-05-10 14:19:24 -0700

    Initial commit.

---
+++ BBB_calculator/__init__.py
@@ -0,0 +1,7 @@
+"""Implements a BBB score and related helper utilities.
+
+Provides the following sub-packages:
+- BBB_calculator: Provides a BBB score calculator.
+"""
+
+from BBB_calculator.BBB_score import BBB_score
